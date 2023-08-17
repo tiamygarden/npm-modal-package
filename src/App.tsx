944305a8038b0327839fd1b31ec08df0ce7cfcea
tommy <50880262+tiamygarden@users.noreply.github.com>
2023-08-17 19:28:35 +0200

    remove tailwind css inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Modal from "./components/Modal.tsx";
 
 function App() {
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
-import React, {forwardRef, useEffect, useImperativeHandle, useState } from "react"
-import { createPortal } from "react-dom"
+import React, {forwardRef, useEffect, useImperativeHandle, useState} from "react"
+import {createPortal} from "react-dom"
 
 /**
  * Composant Modal.
@@ -18,7 +18,7 @@ type ModalProps = {
 };
 
 
-const Modal = forwardRef(({ title, body, footer }:ModalProps, ref) => {
+const Modal = forwardRef(({title, body, footer}: ModalProps, ref) => {
     // Utilisation de "useState" pour gérer l'affichage de la modale
     const [display, setDisplay] = useState(false)
 
@@ -33,7 +33,7 @@ const Modal = forwardRef(({ title, body, footer }:ModalProps, ref) => {
     }
 
     // Utilisation de "useImperativeHandle" pour exposer une API impérative à partir d'un composant fonctionnel
-    useImperativeHandle(ref, () => ({ open, close }))
+    useImperativeHandle(ref, () => ({open, close}))
 
     // Utilisation de "useEffect" pour gérer le défilement du body
     useEffect(() => {
@@ -51,24 +51,41 @@ const Modal = forwardRef(({ title, body, footer }:ModalProps, ref) => {
 
     return createPortal(
         <div
-            className={
-                "fixed inset-0 justify-center items-center bg-black/50 backdrop-blur-sm	 " +
-                (display ? "flex" : "hidden")
-            }
+            style={{
+                position: 'fixed',
+                inset: 0,
+                display: display ? "flex" : "none",
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                backdropFilter: 'blur(4px)'
+            }}
             onClick={() => close()}
         >
             <div
-                className="w-[450px] min-h-[150px] p-2 bg-white rounded-md shadow-2xl flex flex-col overflow-hidden "
+                style={{
+                    width: '450px',
+                    minHeight: '150px',
+                    padding: '0.5rem',
+                    backgroundColor: '#fff',
+                    borderRadius: '0.375rem',
+                    boxShadow: 'box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25)',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    overflow: 'hidden'
+                }}
                 onClick={(e) => e.stopPropagation()}
             >
-                <h3 className="mx-auto pb-2 text-2xl">{title}</h3>
-                <div className="flex flex-wrap flex-grow items-center leading-5 mb-1 ">
+                <h3 style={{ marginLeft: 'auto', marginRight: 'auto', paddingBottom: '.5rem', fontSize: '1.5rem'}}>
+                    {title}
+                </h3>
+                <div style={{ display:'flex', flexWrap:'wrap', flexGrow:'1', justifyItems:'center', lineHeight: '1.25rem', marginBottom: '0.25rem' }} >
                     {body}
                 </div>
-                <div className="bg-stone-100 p-2 -m-2 mt-2 ">
+                <div style={{ backgroundColor: 'rgb(245 245 244)', padding :'.5rem', margin:'.5rem -0.5rem -0.5rem -0.5rem' }}>
                     {footer}
                     <button
-                        className="flex justify-center m-auto p-2 bg-green-500 rounded "
+                        style ={{ display: 'flex', justifyContent:'center', margin:'auto', padding:'.5rem', backgroundColor:'rgb(34 197 94)', borderRadius: '.25rem' }}
                         onClick={() => close()}
                     >
                         Close
